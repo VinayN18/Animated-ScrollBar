b0936e8d0fdd9ae8ece72a7559a4a061c0a01129
Vinay Nakkina <vinay.1@iitj.ac.in>
2023-05-31 09:38:20 +0530

    Seventh Page and Up down animation done

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,8 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { PathComponent } from '../path/path.component';
+import { Component } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-mainpage',
@@ -7,6 +10,57 @@ import { PathComponent } from '../path/path.component';
   styleUrls: ['./mainpage.component.css'],
 })
 export class MainpageComponent {
-  desiredXPosition: number = 250;
-  desiredYPosition: number = 250;
+  ngAfterViewInit() {
+    gsap.utils.toArray('.section').forEach((section: any) => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section,
+          pin: true, // Pin the section to keep it fixed on the screen during the animation
+          start: 'top top', // Animation starts when the top of the section reaches the top of the viewport
+          end: 'bottom top', // Animation ends when the bottom of the section reaches the top of the viewport
+          scrub: true, // Smooth animation during scrolling
+          markers: true, // Add markers for debugging (optional)
+        },
+      });
+
+      tl.from(section.querySelectorAll('.paragraph, .images'), {
+        y: 100, // Move the paragraphs and images 100 pixels vertically
+        opacity: 0, // Fade in the paragraphs and images
+        duration: 1, // Animation duration
+        stagger: 0.2, // Add stagger effect for multiple elements
+      });
+
+      tl.to(section.querySelectorAll('.paragraph, .images'), {
+        y: 0, // Reverse the animation to bring the paragraphs and images back to their original position
+        opacity: 1, // Reverse the opacity animation
+        duration: 1, // Animation duration
+        stagger: 0.2, // Add stagger effect for multiple elements
+      });
+
+      tl.from(section.querySelectorAll('.imagee'), {
+        y: -100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.2,
+      });
+      tl.to(section.querySelectorAll('.imagee'), {
+        y: 0, // Reverse the animation to bring the paragraphs and images back to their original position
+        opacity: 1, // Reverse the opacity animation
+        duration: 1, // Animation duration
+        stagger: 0.2, // Add stagger effect for multiple elements
+      });
+      tl.from(section.querySelectorAll('.im'), {
+        x: 100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.2,
+      });
+      tl.to(section.querySelectorAll('.im'), {
+        x: 0, // Reverse the animation to bring the paragraphs and images back to their original position
+        opacity: 1, // Reverse the opacity animation
+        duration: 5, // Animation duration
+        stagger: 0.2, // Add stagger effect for multiple elements
+      });
+    });
+  }
 }
